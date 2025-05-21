1567a2be7d431bdd94ec0846919a574ec66b6dd5
fabio-Silva-E <fs2894488@gmail.com>
2025-05-21 00:14:01 -0300

    commit now

--- app/main.py
+++ app/main.py
@@ -9,30 +9,13 @@ def format_linter_error(error: dict) -> dict:
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {"errors": [{"line": error["line_number"],
-                        "column": error["column_number"],
-                        "message": error["text"],
-                        "name": error["code"],
-                        "source": "flake8"} for error in errors],
-            "path": file_path,
-            "status": "failed"}
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [{"errors": [],
-             "path": file_path,
-             "status": "passed"
-             } if not errors else {
-        "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            }
-            for error in errors
-        ],
-        "path": file_path,
-        "status": "failed"
-    } for file_path, errors in linter_report.items()]
+    return [format_single_linter_file(file_path, errors)for file_path,
+            errors in linter_report.items()]
